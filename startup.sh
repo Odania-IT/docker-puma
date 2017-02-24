4a8fb548fa90dd090a7a4e82ab8a360e67c4975d
Mike Petersen <mike@odania-it.de>
2017-02-24 22:45:39 +0100

    Script should be started as user app already

--- startup.sh
+++ startup.sh
@@ -5,9 +5,9 @@ cd /srv/app
 if [ ! -z "$PROCESS_TYPE" ] && [ $PROCESS_TYPE == 'background_worker' ]
 then
 	echo "Starting background worker"
-	exec chpst -u app:app bundle exec sidekiq -e ${RAILS_ENV}
+	exec bundle exec sidekiq -e ${RAILS_ENV}
 else
 	echo "Starting puma web process"
-	exec chpst -u app:app bundle exec puma -e ${RAILS_ENV} -p 3000 --threads 2:16
+	exec bundle exec puma -e ${RAILS_ENV} -p 3000 --threads 2:16
 fi
 
